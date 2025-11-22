85b77a36e6b1d4f0425dfdc6c030820bddd15510
claycot <9454608+claycot@users.noreply.github.com>
2025-11-22 17:50:27 -0500

    [claycot] Complete Day 9 Part 1.

--- 2015/day09/puz1.py
+++ 2015/day09/puz1.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 def main():
     # keep an ordered list of when towns were seen on the input
@@ -10,7 +11,7 @@ def main():
     temp_dirs = []
 
     # parse input to the above structures
-    with open('input_short.txt') as file:
+    with open('input.txt') as file:
         for line in file:
             # enforce that the line has loc_1, loc_2, and dist
             direction = re.fullmatch("^(?P<loc_1>[a-zA-Z]*?) to (?P<loc_2>[a-zA-Z]*?) = (?P<dist>[0-9]*?)$", line.rstrip())
@@ -39,10 +40,21 @@ def main():
         dist_matrix[dir[0]][dir[1]] = dir[2]
         dist_matrix[dir[1]][dir[0]] = dir[2]
     
-    # TODO: permute through all of the ways to visit destinations
-    print(dist_matrix)
+    # print("printing distance matrix")
+    # print(dist_matrix)
 
-    return 0
+    paths = find_all_paths(len(town_lookup))
+    # print("printing paths between destinations")
+    # print(paths)
+
+    shortest_dist = math.inf
+    for path in paths:
+        dist = measure_path_distance(dist_matrix, path)
+        if dist < shortest_dist:
+            shortest_dist = dist
+
+    print(f"the shortest distance is {shortest_dist}")
+    return shortest_dist
 
 def find_town_index(lookup: list[int], town: str) -> int:
     t = -1
@@ -52,6 +64,35 @@ def find_town_index(lookup: list[int], town: str) -> int:
         print(f"town {town} was not found in lookup table")
 
     return t
+
+def find_all_paths(num_destinations: int) -> list[list[int]]:
+    paths = [[-1]]
+
+    # visit all destinations in every path
+    for _ in range(num_destinations):
+        new_paths = []
+
+        # for each existing path, make all versions of it that visit a new destination
+        for path in paths:
+            # print(f"appending to path {path}")
+            for dest in range(num_destinations):
+                # print(f"checking if dest {dest} is in path")
+                if dest not in path:
+                    new_path = path[:]
+                    new_path.append(dest)
+                    new_paths.append(new_path)
+
+        paths = new_paths
+
+    return paths
+
+def measure_path_distance(dists: list[list[int]], path: list[int]) -> int:
+    dist = 0
+
+    for i in range(1, len(path) - 1):
+        dist += dists[path[i]][path[i + 1]]
+
+    return dist
 
 if __name__ == "__main__":
     main()
