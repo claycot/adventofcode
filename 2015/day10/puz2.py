04e0309dbc1dbf776e7a1bbec51fd6fef995b9ba
claycot <9454608+claycot@users.noreply.github.com>
2025-11-22 21:38:29 -0500

    [claycot] Complete Day 10.

---
+++ 2015/day10/puz2.py
@@ -0,0 +1,34 @@
+def main():
+    pattern = ''
+    with open("input.txt") as file:
+        for line in file: 
+            pattern = line.rstrip()
+
+    for _ in range(50):
+        pattern = say_hear(pattern)
+
+    print(len(pattern))
+    return 0
+
+# two pointers approach to turn the string into hearsay
+def say_hear(pattern: str) -> str:
+    hearsay = ''
+    i = 0
+
+    # make sure the second pointer is within the string
+    for j in range(len(pattern)):
+        # print(f"pointers are i={i} and j={j}; char at i is {pattern[i]}")
+        # if the chars don't match between the pointers, hear what they say
+        if pattern[i] != pattern[j]:
+            hearsay += str(j - i)
+            hearsay += pattern[i]
+            i = j
+
+    # need to add 1 here because j won't equal full length of string
+    hearsay += str(j - i + 1)
+    hearsay += pattern[i]
+
+    return hearsay
+
+if __name__ == "__main__":
+    main()
