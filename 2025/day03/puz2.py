a7f11c8ea3429f8d5099d732215cf6ab5ebf2348
claycot <9454608+claycot@users.noreply.github.com>
2025-12-03 22:39:25 -0500

    [claycot] Refactor 2025 Day 3 Part 2.

--- 2025/day03/puz2.py
+++ 2025/day03/puz2.py
@@ -1,9 +1,10 @@
 def main(): 
+    JOLTAGE_STRING_LENGTH = 12
     sum_joltages = 0
 
     with open("input.txt") as file:
         for line in file:
-            sum_joltages += find_max_joltage(line.rstrip(), 12)
+            sum_joltages += find_max_joltage(line.rstrip(), JOLTAGE_STRING_LENGTH)
     
     print(sum_joltages)
     return sum_joltages
@@ -12,22 +13,26 @@ def find_max_joltage(line: str, num_digits: int) -> int:
     digits = []
 
     for c in line:
-        c_int = int(c)
+        # append the digit to the end
+        digits.append(int(c))
 
-        # if we don't have a full joltage list, add the next digit to the end
-        if len(digits) < num_digits:
-            digits.append(c_int)
-        # otherwise, remove the first digit in the list that's smaller than the next
-        # and shift everything up, adding the new digit to the end
-        else:
-            digits.append(c_int)
+        # if we don't have too many elements, do nothing!
+
+        # otherwise, look for a digit in the list that's smaller than the next
+        # so it can be removed and everything else be shifted up
+        if len(digits) == (num_digits + 1):
+            # track whether or not we removed a digit
             digit_popped = False
+
+            # look for a digit that's smaller than the next
             for i in range(len(digits) - 1):
-                # if the digit is smaller than the next digit, shift the array then break
+                # if found, remove it and shift everything left
                 if digits[i] < digits[i + 1]:
                     digits.pop(i)
+                    # track that we removed a digit then stop looking
                     digit_popped = True
                     break
+
             # if a digit was not removed, remove the last digit from the end
             if not digit_popped:
                 digits.pop()
