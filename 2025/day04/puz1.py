84cfde6fb596c42079bd301b14b935399893cd27
claycot <9454608+claycot@users.noreply.github.com>
2025-12-04 22:42:26 -0500

    [claycot] Complete 2025 Day 4 Part 1.

---
+++ 2025/day04/puz1.py
@@ -0,0 +1,59 @@
+
+CHARACTER_FULL = '@'
+CHARACTER_EMPTY = '.'
+
+def main():
+    accessible = 0
+
+    grid = []
+    with open("input.txt") as file:
+        for line in file:
+            next_grid = []
+            for char in line.rstrip():
+                if char == CHARACTER_EMPTY:
+                    next_grid.append(0)
+                elif char == CHARACTER_FULL:
+                    next_grid.append(1)
+            grid.append(next_grid)
+
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            accessible += (1 if is_roll_reachable(grid, [r, c]) else 0)
+        
+    # print(grid)
+    print(accessible)
+    return 0
+
+def is_roll_reachable(grid, cell) -> bool:
+    DIRS = [
+        [-1, -1],
+        [-1, 0], 
+        [-1, 1],
+        [0, -1],
+        [0, 1],
+        [1, -1],
+        [1, 0],
+        [1, 1],
+    ]
+
+    [row, col] = cell
+    if not is_valid(grid, cell) or grid[row][col] == 0:
+        return False
+    
+    count_neighbors = 0
+    for dir in DIRS:
+        [delta_row, delta_col] = dir
+        next_row = row + delta_row
+        next_col = col + delta_col
+
+        if is_valid(grid, [next_row, next_col]) and grid[next_row][next_col] == 1:
+            count_neighbors += 1
+
+    return count_neighbors < 4
+
+def is_valid(grid, cell) -> bool:
+    [row, col] = cell
+    return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0])
+
+if __name__ == "__main__":
+    main()
