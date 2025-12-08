a359fd58bfe3f1ca1682ba1db0c58ec31bd3a47f
claycot <9454608+claycot@users.noreply.github.com>
2025-12-07 19:40:11 -0500

    [claycot] Refactor 2025 Day 6 Part 2.

--- 2025/day06/puz2.py
+++ 2025/day06/puz2.py
@@ -1,62 +1,31 @@
 OPERATOR_LIST = "+*"
+WHITESPACE_LIST = "\r\n\t"
 
 def main():
     lines = []
-    prob_widths = []
-    op_line = -1
     sum = 0
 
     with open("input.txt") as file:
         for line in file:
-            lines.append(line)
+            lines.append(line.strip(WHITESPACE_LIST))
 
-    for i in range(len(lines) - 1, 0, -1):
-        if lines[i][0] in OPERATOR_LIST:
-            op_line = i
-            prob_widths = find_widths(lines[i])
-            break
-        elif lines[i].strip():
-            raise ValueError("invalid input; final non-blank line is not operators")
-        
-    if len(prob_widths) == 0:
-        raise ValueError("invalid input; no problems provided")
-    
-    # need the first offset to be -1 since there was no space consumed
-    offset = -1
-    for width in prob_widths:
-        vals = []
+    vals = []
+    for i in range(len(lines[0]) - 1, -1, -1):
+        val = ""
+        for line in lines:
+            if line[i] in OPERATOR_LIST:
+                vals.append(int(val))
+                sum += solve_problem(vals, line[i])
+                val = ""
+                vals = []
+            elif line[i] != " ":
+                val += line[i]
 
-        # jump to the last col in the problem so we can work backwards
-        # +1 is to account for space between problems
-        offset += width + 1
-
-        # starting at the last col and working backwards, build col values
-        for i in range(offset - 1, offset - width - 1, -1):
-            sum_col = ""
-            for line in lines[:op_line]:
-                if line[i] != " ":
-                    sum_col += line[i]
-            vals.append(int(sum_col))
-
-        sum += solve_problem(vals, lines[-1][offset - width])
+        if len(val):
+            vals.append(int(val))
 
     print(sum)
     return sum
-
-def find_widths(op_line: str) -> list[int]:
-    widths = []
-    i_last_op = 0
-
-    for i in range(1, len(op_line)):
-        if op_line[i] in OPERATOR_LIST:
-            widths.append(i - i_last_op - 1)
-            i_last_op = i
-        elif op_line[i] == "\n":
-            widths.append(i - i_last_op)
-            i_last_op = i
-
-    return widths
-
 
 def solve_problem(vals: list[int], operator: str) -> int:
     ans = 0
@@ -66,6 +35,7 @@ def solve_problem(vals: list[int], operator: str) -> int:
     elif (operator == "*"):
         ans = product(vals)
 
+    # print(f"solution to {operator} problem with vals {vals} is {ans}")
     return ans
 
 def product(values: list[int]) -> int:
